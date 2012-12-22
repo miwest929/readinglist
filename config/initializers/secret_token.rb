fc7b0af624bc7195c9df7f841c469ce40daa6627
Mike West <mwest@mdsol.com>
2012-12-22 15:54:32 -0500

    Initial commit for project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HypermediaDemo::Application.config.secret_token = 'd22544ce60ade74c793361a40eb49438d2e87b59b03a232fb8daf3b8233f7e0f3363c9e306b3912dbd95a8b70d3bd51b61f35cb1d18680c83215eb4cdd802eab'
